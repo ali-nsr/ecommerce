6c61bcfb6dcc8d66caf1f47d4b2e09243fb3a9d4
dream1234night@gmail.com <dream1234night@gmail.com>
2024-06-03 20:56:08 +0330

    ver-1

--- config/urls.py
+++ config/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('', include('shop.urls', namespace='shop')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings as conf_settings
+from django.conf.urls.static import static
+
+if conf_settings.DEBUG:
+    urlpatterns += static(conf_settings.STATIC_URL, document_root=conf_settings.STATIC_ROOT)
+    urlpatterns += static(conf_settings.MEDIA_URL, document_root=conf_settings.MEDIA_ROOT)
